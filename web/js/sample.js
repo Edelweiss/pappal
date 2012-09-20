1207f43baab511627bdd991dea64b83ec184f23c
Carmen Maria Lanz (Aquila) <elemmire@ossiriand.de>
2012-09-20 08:01:37 +0200

    ignore version number when setting thumbnail as preview thumbnail, outsource onclick event to js file

--- web/js/sample.js
+++ web/js/sample.js
@@ -1,5 +1,12 @@
 $(function(){
 
+  // master thumbnail
+  $('img.sampleMini').click(function(){
+    var image = $(this).attr('src');
+    $('#sample').attr('src', image);
+    $('#masterThumbnail').val(image.match(/\/([^\/]+\.jpg)(\?\d+)?/)[1]);
+  });
+
   // delete action
   $('#buttonDelete').click(function(){
     if(confirm('Are you sure you want to delete this record?\nThis action cannot be undone.')){
