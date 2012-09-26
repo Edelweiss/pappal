70083eeffe0fd4dbd3730d9f0d9e5e9b60d1ef26
Carmen Maria Lanz (Aquila) <elemmire@ossiriand.de>
2012-09-26 15:59:38 +0200

    dummy homepage

--- src/Papyrillio/PapPalBundle/Controller/DefaultController.php
+++ src/Papyrillio/PapPalBundle/Controller/DefaultController.php
@@ -7,9 +7,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    
     public function indexAction($name)
     {
         return $this->render('PapyrillioPapPalBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function homeAction()
+    {
+        return $this->render('PapyrillioPapPalBundle:Default:home.html.twig');
     }
 }
