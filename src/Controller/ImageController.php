573d3db359f831a0232f122b423709ef7c2b9000
Elor Oké (Piccolo) <elor.oke@adamsakte.de>
2021-03-07 18:48:23 +0100

    straighten-out typo

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\Sample;
-use App\Entity\Commet;
+use App\Entity\Comment;
 use App\Entity\User;
 
 
