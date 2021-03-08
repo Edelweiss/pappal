2d540a766431fa6225a1ce6eb9bdc30ed672d41b
Elor Oké (Piccolo) <elor.oke@adamsakte.de>
2021-03-08 22:38:24 +0100

    add forms for thumbnails and samples

---
+++ src/Form/Type/SampleType.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Sample;
+
+class SampleType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
+      $builder->add('hgv', TextType::class, ['required' => false]); // e.g. 1915a
+      $builder->add('ddb', TextType::class, ['required' => false]); // e.g. bgu
+      $builder->add('dateWhen', TextType::class, ['required' => false]); 
+      $builder->add('dateNotBefore', TextType::class, ['required' => false]);
+      $builder->add('dateNotAfter', TextType::class, ['required' => false]);
+      $builder->add('title', TextType::class, ['required' => false]);
+      $builder->add('material', ChoiceType::class, ['choices' => ['Papyrus' => 'Papyrus', 'Ostrakon' => 'Ostrakon', 'tafel' => 'Tafeln'], 'preferred_choices' => [''], 'required' => false]);
+      $builder->add('keywords', TextType::class, ['required' => false]);
+      $builder->add('provenance', TextType::class, ['required' => false]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void {
+        $resolver->setDefaults([
+            'data_class' => Sample::class,
+        ]);
+    }
+
+    /*public function getDefaultOptions(array $options)
+    {
+        return [
+            'data_class' => 'Papyrillio\PapPalBundle\Entity\Sample',
+        ];
+    }
+
+    public function getName()
+    {
+        return 'sample';
+    }*/
+}
---
+++ src/Form/Type/ThumbnailType.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Form\Type\SampleType;
+use App\Entity\Thumbnail;
+
+class ThumbnailType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
+        $builder->add('sample', SampleType::class);
+        $builder->add('language', ChoiceType::Class, ['choices' => ['grc' => 'Griechisch', 'lat' => 'Lateinisch'],
+                                                      'preferred_choices' => array(''),
+                                                      'required' => false]);
+        //$builder->add('template', null, array('property_path' => false));
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void {
+        $resolver->setDefaults([
+            'data_class' => Thumbnail::class,
+        ]);
+    }
+    
+    /*public function getDefaultOptions(array $options)
+    {
+        return array(
+            'data_class' => 'Papyrillio\PapPalBundle\Entity\Thumbnail',
+        );
+    }
+
+    public function getName()
+    {
+        return 'thumbnail';
+    }*/
+}
