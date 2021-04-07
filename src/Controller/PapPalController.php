26f154f284524c1312ea11bd2b718352ee91830c
Elor Oké (Piccolo) <elor.oke@adamsakte.de>
2021-04-07 08:20:07 +0200

    comment for parameter hot fix

--- src/Controller/PapPalController.php
+++ src/Controller/PapPalController.php
@@ -23,7 +23,7 @@ class PapPalController extends AbstractController{
   }
 
   protected function getParameter($key){
-    if(parent::getParameter($key)){ // Symfony Container Parameters
+    if(parent::getParameter($key)){ // cl: Symfony Container Parameters
       return parent::getParameter($key);
     }
     if(array_key_exists($key, $this->allParameters)){ // GET & POST
