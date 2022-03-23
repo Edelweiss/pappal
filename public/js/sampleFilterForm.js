cc6971588d387ff8cb7848e0bf904dd8debc57a8
Elor Oké (Piccolo) <elor.oke@adamsakte.de>
2022-03-23 08:27:54 +0100

    reload thumbnail list after clearing form fields

--- public/js/sampleFilterForm.js
+++ public/js/sampleFilterForm.js
@@ -1,15 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $('#reset').click(function(){
-    var form = $('#search');
-    form.find("input[name^='thumbnail'][type!='hidden']").each(function(index, input){
-      $(input).val('');
+    $('#reset').click(function() {
+        var form = $('#search');
+        form.find("input[name^='thumbnail'][type!='hidden']").each(function(index, input) {
+            $(input).val('');
+        });
+        form.find("#thumbnail_sample_material").val('');
+        form.find("#thumbnail_language").val('');
+        form.find("select[name^='sort']").val('');
+        form.find("#submit").click();
+
+        return false;
     });
-    form.find("#thumbnail_sample_material").val('');
-    form.find("#thumbnail_language").val('');
-    form.find("select[name^='sort']").val('');
-
-    return false;
-  });
 
 });
